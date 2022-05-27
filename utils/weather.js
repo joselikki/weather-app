3aac595bdebfbe684b88d5b4b007235c27fd49f9
Jose Paredes <jozemparedes@gmail.com>
2022-05-26 23:24:57 -0600

    weather funct complete

---
+++ utils/weather.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const axios = require('axios')
+
+const weatherUrl = "http://api.weatherstack.com"
+const weatherToken = process.env.WEATHER_TOKEN
+
+const weather = async (lat, long, callback) => {
+    const url = `${weatherUrl}/current?access_key=${weatherToken}&query=${lat},${long}`
+    try{
+        const response  = await axios.get(url)
+
+        if (response.data.success === false){
+            callback("Unable to find location, please check!", undefined)
+        } else {
+            callback(undefined, response.data)
+        }
+
+    }catch(err){
+        callback("Unable to connect to weather service!" + err, undefined)
+    }
+}
+module.exports = weather;
