fa356232a39387cdbcd122e1c93fd13827f63569
Jose Paredes <jozemparedes@gmail.com>
2022-05-28 00:39:47 -0600

    display results

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,14 +1,46 @@
 <script>
+import { dataset_dev } from "svelte/internal";
+
+
+
+	let address
+	let showData = true
+
+	let mockData = {
+		location : "Guatemala, Guatemala City",
+		temperature : 25,
+		weather: 'Cloudy'
+	}
+
+	const handleSubmit = (e) =>{
+		showData = false
+		showData = true
+	}
 
 </script>
 
 <main>
 	<h1>Weather</h1>
 
-	<form >
+	<form on:submit|preventDefault ={handleSubmit} >
 		
-		<input placeholder="Location" type="text">
+		<input 
+			bind:value={address}
+			placeholder="Location" 
+			type="text"
+		>
 		<button>Search</button>
+
+		{#if showData}
+		<div class="results">
+			<div class="data">
+				<p>Location: {mockData.location}</p>
+				<p>Temperature: {mockData.temperature}</p>
+				<p>Weather: {mockData.weather}</p>
+			</div>
+		</div>
+			{/if}
+
 
 	</form>
 </main>
@@ -17,7 +49,7 @@
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
+		max-width: 650px;
 		margin: 3em auto;
 	}
 
@@ -27,9 +59,9 @@
 		letter-spacing: -0.03em;
 	}
 
-	@media (min-width: 650px) {
-		main {
-			max-width: none;
-		}
+	.results {
+		width: auto;
+		margin: 0 auto;
 	}
+
 </style>
