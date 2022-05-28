45b7558d1e4b8ca82fdcba614481cc7453a64d19
Jose Paredes <jozemparedes@gmail.com>
2022-05-27 19:24:47 -0600

    Search input and results complete

--- public/js/main.js
+++ public/js/main.js
@@ -4,12 +4,38 @@ const getWeather = async (address, callback) => {
     callback(data)
 }
 
-// // // getWeather("boston", data => {
-// // //     if (data.error){
-// // //         return console.log(data.error)
-// // //     }
-// // //     console.log(`Location: ${data.location}`)
-// // //     console.log(`Weather: ${data.wather}`)
-// // //     console.log(`Location: ${data.temperature}`)
+const watherForm = document.querySelector('form')
+const address = document.getElementById('user-adress')
 
-// // // })
+const errorMsg = document.querySelector('#error')
+const locationMsg = document.querySelector('#location')
+const weatherMsg = document.querySelector('#weather')
+const temperatureMsg = document.querySelector('#temperature')
+const searchingMsg = document.querySelector('#searching')
+
+watherForm.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    searchingMsg.textContent ="Searching ..."
+    errorMsg.textContent = ""
+    locationMsg.textContent = ""
+    weatherMsg.textContent = ""
+    temperatureMsg.textContent = ""
+
+    if(!address.value){
+        searchingMsg.textContent = ""
+        errorMsg.textContent = "Please enter a location."
+    } else {
+
+        getWeather(address.value, data => {
+            searchingMsg.textContent = ""
+            if (data.error) {
+                return errorMsg.textContent = data.error
+            } else {
+
+                locationMsg.textContent = `Location: ${data.location}`
+                weatherMsg.textContent = `Weather: ${data.weather}`
+                temperatureMsg.textContent = `Temperature: ${data.temperature}`
+            }
+        })
+    }
+})
