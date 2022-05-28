b04910ae267ab9569f83f88c704c9af52d194234
Jose Paredes <jozemparedes@gmail.com>
2022-05-28 00:13:04 -0600

    Tile and input added

---
+++ client/src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+
+</script>
+
+<main>
+	<h1>Weather</h1>
+
+	<form >
+		
+		<input placeholder="Location" type="text">
+		<button>Search</button>
+
+	</form>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 3em auto;
+	}
+
+	h1 {
+		font-size: 3em;
+		font-weight: bold;
+		letter-spacing: -0.03em;
+	}
+
+	@media (min-width: 650px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
